641519e280245ef322aae3bd08d4fe3ec18ae655
Rizki Ardian <radeite8@gmail.com>
2017-03-31 21:38:45 +0700

    - Removed spaceship theme - Removed pure - Changed tint2 clock format

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -5,6 +5,3 @@ source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Load k
 source ~/.zsh/plugins/k/k.sh
-
-# Load spaceship
-source ~/.zsh/plugins/spaceship-zsh-theme/spaceship.zsh-theme
